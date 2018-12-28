157a1944b9d1c4641d5010d7529b7ddc784d4b77
Shirinay Kurban <shirinayk81@gmail.com>
2018-12-28 00:12:26 -0500

    added new work

---
+++ ShirinayK/Homework/HW2/MySlogan.java
@@ -0,0 +1,12 @@
+
+public class MySlogan {
+
+	public static void main(String[] args) {
+
+		String newString = "    You can always start where you are and change the ending!   ";
+		
+		System.out.println(newString.trim());
+
+	}
+
+}
