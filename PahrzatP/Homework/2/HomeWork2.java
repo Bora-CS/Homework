8dfc676e152db694913e95ce9e8c8d41069f830f
PahrzatParhat <pharfar5@gmail.com>
2018-12-28 07:52:42 -0800

    Added

---
+++ PahrzatP/Homework/2/HomeWork2.java
@@ -0,0 +1,18 @@
+public class HomeWork2{
+    public static void main(String[] args){
+        Scanner myScan = new Scanner(System.in);
+		System.out.println("Input Anything: ");
+		String myString = myScan.nextLine();
+		String myReversedString = reverseString1(myString);
+		System.out.println("Reversed Input: \n" + myReversedString);
+    }
+    private static String reverseString1(String myString) {
+		char[] myCharArr = myString.toCharArray();
+		String result = "";
+		for (int a = 0; a < myCharArr.length; a++) {
+			result = myCharArr[a] + result;
+		}
+
+		return result;
+	}
+}
