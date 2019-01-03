6221b0becc0eb21579a3864dd9591f60da1b18f3
kirajie <jam1993625@gmail.com>
2019-01-03 11:25:40 -0500

    format my file

--- RaidenZ/HomeWork/3/Unit1.java
+++ RaidenZ/HomeWork/3/Unit1.java
@@ -13,76 +13,58 @@ import org.testng.annotations.Test;
 
 import com.thoughtworks.selenium.Wait;
 
-	public class Unit1 {
-		WebDriver driver;
-		@Test(priority = 0)
-		public void firstTest() {
-			
-			driver.get("https://www.amazon.com");
-			driver.findElement(By.id("twotabsearchtextbox")).sendKeys("phone");
-			driver.findElement(By.xpath("//input[@value='Go']")).click();
-			WebElement dropDown = driver.findElement(By.xpath("//select[@id='sort']"));
-			Select mySel = new Select(dropDown);
-			
-			mySel.selectByValue("price-desc-rank");
-		
-			
-			
-			
-			sleep(5);
-			
-			
-			
+public class Unit1 {
+	WebDriver driver;
+
+	@Test(priority = 0)
+	public void firstTest() {
+		driver.get("https://www.amazon.com");
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("phone");
+		driver.findElement(By.xpath("//input[@value='Go']")).click();
+		WebElement dropDown = driver.findElement(By.xpath("//select[@id='sort']"));
+		Select mySel = new Select(dropDown);
+		mySel.selectByValue("price-desc-rank");
+		sleep(5);
 	}
+
 	@Test(priority = 1)
 	public void secondTest() {
 		driver.get("https://www.amazon.com");
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("macbook");
 		driver.findElement(By.xpath("//input[@value='Go']")).click();
-		driver.findElement(By.xpath("//a[contains(@title,'Apple MacBook Air (13-inch Retina display, 1.6GHz dual-core Intel Core i5, 128GB) - Silver (Latest Model)')]//h2[contains(@class,'a-text-normal')]")).click();
+		driver.findElement(By.xpath(
+				"//a[contains(@title,'Apple MacBook Air (13-inch Retina display, 1.6GHz dual-core Intel Core i5, 128GB) - Silver (Latest Model)')]//h2[contains(@class,'a-text-normal')]"))
+				.click();
 		driver.findElement(By.xpath("//button[@id='a-autoid-18-announce']")).click();
 		driver.findElement(By.xpath("//button[@id='a-autoid-19-announce']//div//div//img[@class='imgSwatch']")).click();
 		sleep(5);
 		driver.findElement(By.xpath("//input[@id='add-to-cart-button']")).click();
 		driver.findElement(By.xpath("//a[@id='nav-cart']")).click();
-		if(!driver.findElement(By.xpath("//span[@class='a-size-medium sc-product-title a-text-bold']")).isDisplayed()) {
+		if (!driver.findElement(By.xpath("//span[@class='a-size-medium sc-product-title a-text-bold']"))
+				.isDisplayed()) {
 			System.out.println("Item in cart doesn't match.");
 		}
 	}
-	
-	
+
 	@BeforeTest
 	public void startBrowser() {
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-		 driver = new ChromeDriver();
+		driver = new ChromeDriver();
 	}
-	
-	
-	
+
 	@AfterTest
 	public void endtBrowser() {
 		driver.close();
 		driver.quit();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
-	
-	
-	
 	public static void sleep(int seconds) {
 		try {
-			Thread.sleep(seconds*1000);
+			Thread.sleep(seconds * 1000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 }
