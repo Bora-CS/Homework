1f53bd86ef518616214e3b4bf468e3c0f2a6ea97
jimmy Chung <chungj868@gmail.com>
2019-01-09 00:29:50 -0500

    make some change on test

--- src/test/java/JimmyC/RefreshBrower.java
+++ src/test/java/JimmyC/RefreshBrower.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RefreshBrower {
-
+     
 	public static void main(String[] args) throws InterruptedException {
-
+		
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\jcruk\\Homework\\src\\test\\resources\\Drivers\\chromedriver.exe");
 		WebDriver myDriver = new ChromeDriver();
