770aa7942ea6759da2332e0e64f0d0ea84640d3b
pk <pk@192.168.1.107>
2019-01-09 02:59:17 -0500

    reusable method in Keywords class and made package with first test case

--- src/main/java/utilities/Keywords.java
+++ src/main/java/utilities/Keywords.java
@@ -5,7 +5,7 @@ import static utilities.DriverFactory.driver;
 import org.openqa.selenium.By;
 
 public class Keywords {
-	
+
 	public static void sleep(int seconds) {
 		try {
 			Thread.sleep(seconds * 1000);
@@ -13,25 +13,32 @@ public class Keywords {
 			System.out.println(e.getMessage());
 		}
 	}
-	
+
 	public static void startTest(String url) {
 		DriverFactory.startChromeDriver();
 		driver.navigate().to(url);
 	}
-	
+
 	public static void endTest() {
 		DriverFactory.stopChromeDriver();
 	}
-	
+
 	public static void print(String message) {
 		System.out.println(message);
 	}
-	
-	public static void click(By locator) {	
+
+	public static void click(By locator) {
 		driver.findElement(locator).click();
 	}
-	
+
 	public static void input(By locator, String keysToSend) {
 		driver.findElement(locator).sendKeys(keysToSend);
 	}
+
+	// Sergio
+	public static void back() {
+		driver.navigate().back();
+		
+	}
+
 }
