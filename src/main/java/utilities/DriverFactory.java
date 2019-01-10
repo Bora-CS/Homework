de9782d1599067b0e542068d0891e13c3410b7b8
Muradil Erkin <Miller.muradil@gmail.com>
2019-01-09 20:21:39 -0500

    Bug fix and some improvement

--- src/main/java/utilities/DriverFactory.java
+++ src/main/java/utilities/DriverFactory.java
@@ -8,15 +8,38 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DriverFactory {
 	public static WebDriver driver;
 	
-	public static void startChromeDriver () {
-		System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH_MAC);
+	public static void startChromeDriver () throws Exception {
+		String driverPath = determineOSandGetDriverPath();
+		System.setProperty("webdriver.chrome.driver", driverPath);
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+
+	private static String determineOSandGetDriverPath() throws Exception {
+		String os = System.getProperty("os.name");
+	    System.out.println("Operating System: " + os);
+	    if (os.toLowerCase().startsWith("mac")) {
+	    	OperatingSystem = "MAC";
+	    	return Constants.CHROME_DRIVER_PATH_MAC;
+	    } else if (os.toLowerCase().startsWith("windows")) {
+	    	OperatingSystem = "WINDOWS";
+	    	return Constants.CHROME_DRIVER_PATH_WINDOWS;
+	    } else {
+	    	OperatingSystem = "UNKNOWN";
+	    	throw new Exception("Unknown Operating System: " + os); 
+	    }
 	}
 
 	public static void stopChromeDriver() {
 		driver.close();
 		driver.quit();
 	}
+	
+	private static String OperatingSystem;
+	
+	public static String getOperatingSystem() {
+		return OperatingSystem;
+	}
+
 }
---
+++ src/test/java/MuradilE/randomTest.java
@@ -0,0 +1,18 @@
+package MuradilE;
+
+public class randomTest {
+
+	public static void main(String[] args) {
+//		System.getenv().forEach((k, v) -> {
+//		    System.out.println(k + ":" + v);
+//		});
+		
+		System.getProperties().forEach((k, v) -> {
+			System.out.println(k + ":" + v);
+		});
+		
+		//user.dir:/Users/Muradil/eclipse-workspace/Homework
+		
+	}
+
+}
