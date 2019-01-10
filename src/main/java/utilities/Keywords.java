04ce96895123750fb09a07e79cdffa4dce60f381
jimmy Chung <chungj868@gmail.com>
2019-01-09 23:39:37 -0500

    format the file

--- src/main/java/utilities/Keywords.java
+++ src/main/java/utilities/Keywords.java
@@ -5,7 +5,7 @@ import static utilities.DriverFactory.driver;
 import org.openqa.selenium.By;
 
 public class Keywords {
-	
+
 	public static void sleep(int seconds) {
 		try {
 			Thread.sleep(seconds * 1000);
@@ -13,39 +13,33 @@ public class Keywords {
 			System.out.println(e.getMessage());
 		}
 	}
-	
+
 	public static void startTest(String url) {
 		DriverFactory.startChromeDriver();
 		driver.navigate().to(url);
 	}
-	
+
 	public static void endTest() {
 		DriverFactory.stopChromeDriver();
 	}
-	
+
 	public static void print(String message) {
 		System.out.println(message);
 	}
-	
-	public static void click(By locator) {	
+
+	public static void click(By locator) {
 		driver.findElement(locator).click();
 	}
-	
+
 	public static void input(By locator, String keysToSend) {
 		driver.findElement(locator).sendKeys(keysToSend);
 	}
 
-// Creat by jimmy
-	public static void refreshBrower(String url){
+	// Creat by jimmy
+	public static void refreshBrower(String url) {
 		driver.navigate().refresh();
 	}
 
-
-
 }
-
-
-
-
 
 
