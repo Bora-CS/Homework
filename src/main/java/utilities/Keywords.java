86c178741311fd9ed36deb814858675863bd0b5d
Muradil Erkin <Miller.muradil@gmail.com>
2019-01-10 20:58:23 -0500

    Changes from class 1/10/2019

--- src/main/java/utilities/Keywords.java
+++ src/main/java/utilities/Keywords.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 
 public class Keywords {
 	
-	public static void sleep(int seconds) {
+	public static void waitFor(int seconds) {
 		try {
 			Thread.sleep(seconds * 1000);
 		} catch (InterruptedException e) {
@@ -37,9 +37,5 @@ public class Keywords {
 	
 	public static void input(By locator, String keysToSend) {
 		driver.findElement(locator).sendKeys(keysToSend);
-	}
-	
-	public static void testMethod () {
-	 // add some more changes 	
 	}
 }
