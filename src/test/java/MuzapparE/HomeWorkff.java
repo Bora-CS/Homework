850c2b08d128d1ec76d7981793c7f10632f57368
Muzappar Erkin <Muzappar.erkin0122@gmail.com>
2019-05-13 00:44:34 -0400

    123

---
+++ src/test/java/MuzapparE/HomeWorkff.java
@@ -0,0 +1,69 @@
+package MuzapparE;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import utilities.Constants;
+import utilities.Keywords;
+
+public class HomeWorkff {
+
+	public static void main(String[] args) {
+		
+		System.out.println(17 % 10);
+//		System.setProperty("webdriver.chrome.driver", "src/test/resources/Drivers/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+//
+//        driver.navigate().to("file:///Users/erkin/eclipse-workspace/Homework/src/test/resources/Drivers/index.html");
+//
+//        Keywords.waitFor(2);
+//
+//
+//        driver.findElement(By.id("firstName")).sendKeys("Shirinay");
+//        driver.findElement(By.id("lastName")).sendKeys("Kurban");
+//        driver.findElement(By.id("email")).sendKeys("kxirnay@gmail.com");
+//        driver.findElement(By.id("phonenumber")).sendKeys("571-666-8888");
+//        driver.findElement(By.id("address")).sendKeys("134 Main St");
+//        
+//        
+//        Select countryOptions = new Select(driver.findElement(By.id("country")));
+//        countryOptions.selectByVisibleText("United States");
+//        
+//        
+//        Select stateOptions = new Select(driver.findElement(By.id("state")));
+//        stateOptions.selectByVisibleText("Virginia");
+//        
+//        driver.findElement(By.id("zip")).sendKeys("22031");
+//        
+////        driver.findElement(By.xpath("//div[6]/label")).click();
+//        driver.findElement(By.className("custom-control-label")).click();
+//        Keywords.waitFor(2);
+//        driver.findElement(By.xpath("//div[7]/label")).click();
+//        
+////        List<WebElement> paymentType = driver.findElements(By.cssSelector(".custom-control.custom-radio"));
+////        paymentType.get(1).findElement(By.tagName("label")).click();
+//        driver.findElement(By.xpath("//div[8]/div[1]/label")).click();
+//        driver.findElement(By.id("cc-name")).sendKeys("aksdhaskjdh");
+//        driver.findElement(By.id("cc-number")).sendKeys("38761873682763");
+//        driver.findElement(By.id("cc-expiration")).sendKeys("12/21");
+//        driver.findElement(By.id("cc-cvv")).sendKeys("123");
+//        Keywords.waitFor(2);
+//        
+//        driver.findElement(By.xpath("/html/body/div/div[2]/div[2]/form/button")).click();
+//        
+//
+//        Keywords.waitFor(2);
+//        
+//        driver.close();
+//        driver.quit();
+//        
+        
+    }
+	}
+
+
